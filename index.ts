84b55db8caf80d9ad8a308499f1fc4ca9d8e20d8
Ajay Saini <ajaysaini@adobe.com>
2022-12-03 13:32:05 +0530

    basic typescript setup

---
+++ index.ts
@@ -0,0 +1,12 @@
+import k8s from '@kubernetes/client-node';
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+k8sApi.listNamespacedPod('default').then((res) => {
+    console.log(res.body);
+});
+
+// console.log('Hello, World!')
